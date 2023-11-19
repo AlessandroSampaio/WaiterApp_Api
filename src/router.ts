42ee0b08c806fee6070c6fb720a68344e63b81a5
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-19 14:06:17 -0300

    Adiciona rotas de listagem e criação de pedidos

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,14 @@
 import path from 'node:path';
+import multer from 'multer';
 
 import { Router } from 'express';
 import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
-
-import multer from 'multer';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
 
 export const router = Router();
 
@@ -38,14 +39,10 @@ router.post('/products',upload.single('image'),  createProduct);
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
 //List orders
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
 
 //Create order
-router.post('/orders', (req, res) => {
-  res.send('OK');
-});
+router.post('/orders', createOrder);
 
 //Change order status
 router.patch('/orders/:orderId', (req, res) => {
