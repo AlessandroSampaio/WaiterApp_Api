ab4ad0dfa228fa6e24febdf75441d7df00c563b9
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-19 11:50:57 -0300

    Define os models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 
-const app = express();
-const port =3001;
 
-app.listen(port, () => console.log(`🚀 Server running on http://localhost:${port}`));
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    console.log('Conectado com sucesso');
+    app.listen(port, () => console.log(`🚀 Server running on http://localhost:${port}`));
+  })
+  .catch((error) => console.log(error));
+
+
